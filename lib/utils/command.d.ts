186acfa3f7463079f738b52e8a959389821e09b7
Emanuele Stoppa <my.burning@gmail.com>
2019-11-08 14:57:45 +0000

    feat: fixed various types and removed unused commands

--- lib/utils/command.d.ts
+++ lib/utils/command.d.ts
@@ -1,16 +1,92 @@
-export interface Command {
-    name: string,
-    alias: string,
-    scope: string,
-    type: any,
-    description: string
+import { OptionDefinition } from 'command-line-args'
+export interface Command  {
+    scope?: string;
+    description?: string
+    /**
+     * The long option name.
+     */
+    name: string;
+
+    /**
+     * A setter function (you receive the output from this) enabling you to be specific about the type and value received. Typical values
+     * are `String` (the default), `Number` and `Boolean` but you can use a custom function. If no option value was set you will receive `null`.
+     */
+    type?: (input: string) => any;
+
+    /**
+     * A getopt-style short option name. Can be any single character except a digit or hyphen.
+     */
+    alias?: string;
+
+    /**
+     * Set this flag if the option accepts multiple values. In the output, you will receive an array of values each passed through the `type` function.
+     */
+    multiple?: boolean;
+
+    /**
+     * Identical to `multiple` but with greedy parsing disabled.
+     */
+    lazyMultiple?: boolean;
+
+    /**
+     * Any values unaccounted for by an option definition will be set on the `defaultOption`. This flag is typically set
+     * on the most commonly-used option to enable more concise usage.
+     */
+    defaultOption?: boolean;
+
+    /**
+     * An initial value for the option.
+     */
+    defaultValue?: any;
+
+    /**
+     * One or more group names the option belongs to.
+     */
+    group?: string | string[];
 }
 
-export interface CoreCommand {
-    type: any,
-    multiple: boolean,
-    defaultValue?: boolean,
-    defaultOption?: boolean,
-    group: string,
-    description: string,
+export interface CoreCommand extends OptionDefinition {
+    name?: string;
+    description?: string,
+    /**
+     * The long option name.
+     */
+    name: string;
+
+    /**
+     * A setter function (you receive the output from this) enabling you to be specific about the type and value received. Typical values
+     * are `String` (the default), `Number` and `Boolean` but you can use a custom function. If no option value was set you will receive `null`.
+     */
+    type?: (input: string) => any;
+
+    /**
+     * A getopt-style short option name. Can be any single character except a digit or hyphen.
+     */
+    alias?: string;
+
+    /**
+     * Set this flag if the option accepts multiple values. In the output, you will receive an array of values each passed through the `type` function.
+     */
+    multiple?: boolean;
+
+    /**
+     * Identical to `multiple` but with greedy parsing disabled.
+     */
+    lazyMultiple?: boolean;
+
+    /**
+     * Any values unaccounted for by an option definition will be set on the `defaultOption`. This flag is typically set
+     * on the most commonly-used option to enable more concise usage.
+     */
+    defaultOption?: boolean;
+
+    /**
+     * An initial value for the option.
+     */
+    defaultValue?: any;
+
+    /**
+     * One or more group names the option belongs to.
+     */
+    group?: string | string[];
 }
